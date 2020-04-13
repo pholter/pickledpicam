a7791fc59659e4c2bee65c3df94681f9799d160f
Peter Holtermann <peter.holtermann@io-warnemuende.de>
2020-04-13 17:21:06 +0200

    install

--- debian_setup/install_rc_script.sh
+++ debian_setup/install_rc_script.sh
@@ -3,3 +3,8 @@ echo "Copying rc_script to init.d folder"
 sudo cp -v pickledpicam_rc /etc/init.d
 echo "Chmodding script"
 sudo chmod -v 755 /etc/init.d/pickledpicam_rc
+echo "Registering script"
+sudo update-rc.d pickledpicam_rc defaults
+unreg="sudo update-rc.d -f pickledpicam_rc remove"
+echo "Un registering script with " $unreg
+
